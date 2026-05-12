a4b5e7cc23036eeade50edae8c5b7e7397fdcfaf
zied oussaifi <ousaifizied@hotmail.fr>
2026-05-12 15:23:41 +0200

    feat: add kafka producer layer

---
+++ telnet-intel-engine/kafka/producer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaProducer
+import json
+
+class TelnetProducer:
+
+    def __init__(self,
+                 bootstrap_servers="localhost:9092"):
+
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        )
+
+    def send(self, topic, payload):
+        self.producer.send(topic, payload)
+        self.producer.flush()
